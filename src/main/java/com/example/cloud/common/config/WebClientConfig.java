4efe4b2dd3367440ff4d99b92c96217ccc397f06
김도영 <zz01064842675@gmail.com>
2025-08-28 13:15:33 +0900

    redis 사용

--- src/main/java/com/example/cloud/common/config/WebClientConfig.java
+++ src/main/java/com/example/cloud/common/config/WebClientConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:20190")
-                .defaultHeaders(httpHeaders -> {
-                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                    httpHeaders.set("Auth-header", "second");
-                })
-                .build();
-    }
+//    @Bean
+//    public WebClient webClient(WebClient.Builder builder) {
+//        return builder.baseUrl("http://kimd0.iptime.org:10100")
+//                .defaultHeaders(httpHeaders -> {
+//                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+//                    httpHeaders.set("Auth-header", "second");
+//                })
+//                .build();
+//    }
 }

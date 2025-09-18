5955e0ac308b07e64dcfe0b082ea39425b7f2f39
김도영 <zz01064842675@gmail.com>
2025-09-18 10:29:20 +0900

    test코드 수정

---
+++ src/test/java/com/example/cloud/CloudApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.cloud;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CloudApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
